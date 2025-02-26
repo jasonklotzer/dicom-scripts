8c5574c2310a93a5fc3877cd4d567a215436b23d
Jason Klotzer <jason@enlyt.com>
2025-02-26 08:31:41 -0600

    fix: missing first day

--- findstudies.sh
+++ findstudies.sh
@@ -33,9 +33,9 @@ echo "Finding studies from $START_DATE to $END_DATE"
 CURR_DATE=$START_DATE
 while : ; do
   echo $CURR_DATE
-  CURR_DATE=$(date +"%Y%m%d" -ud "$CURR_DATE UTC + 1 day")
   findscu -S -k QueryRetrieveLevel=STUDY -k StudyDate="$CURR_DATE" -k NumberOfStudyRelatedInstances -aet SAFEBRIDGE -aec $PACS_HOST -od $TMP_DIR -X
   find $TMP_DIR -name "*.dcm"  | xargs -I % dcm2json -fc % >> $RESP_FILE
   find $TMP_DIR -name "*.dcm" -delete
+  CURR_DATE=$(date +"%Y%m%d" -ud "$CURR_DATE UTC + 1 day")
   [ "$CURR_DATE" -le "$END_DATE" ] || break
 done
